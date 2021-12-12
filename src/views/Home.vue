e9eab9aba5a9b369b94e5343fec198bd0d644630
Prk <71644464+BiliPrk@users.noreply.github.com>
2021-12-12 17:50:19 +0800

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="home">
+        想要查询域名是否授权吗？如果想查询，就<router-link to="/auth">点我</router-link>！
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+    }
+</script>
